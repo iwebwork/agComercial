d9bd86fb4660358983c65782f6e01dd1f99c6ff9
iwebwork <iwebwork.adm@gmail.com>
2019-06-05 21:59:55 -0300

    Erro ao desmarcar evento finalizado

--- php/desmarcarEvento.php
+++ php/desmarcarEvento.php
@@ -1,23 +1,22 @@
 <?php
     include 'classes/usuario.class.php';
 	include 'classes/evento.class.php';
-	include 'classes/email.class.php';
 	include 'classes/pet.class.php';
 	include 'classes/proprietario.class.php';
 	$usuario = new Usuario();
 	$usuario->verificacaoLogin();
 	
     if(!empty($_POST['idConsulta'])){
+        //print_r($_POST);
         $evento = new Eventos();
         $valor = $evento->desmarcarEventos($_POST['idConsulta']);
         if($valor == true){
             header("Location:../index.php");
+            //echo "funcionou";
         }else{
-            echo "Erro não funcionou";
+            
+            //echo "Erro não funcionou";
         }
-    }else{
-
     }
-    
 
 	
