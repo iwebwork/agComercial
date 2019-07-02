b456754583cb35d7aedd8e9ae8cc16813a7f88c0
iwebwork <iwebwork.adm@gmail.com>
2019-07-01 21:23:51 -0300

    charset utf8 adicionado na referancia do banco

--- php/classes/Banco.class.php
+++ php/classes/Banco.class.php
@@ -3,8 +3,7 @@
 		protected $pdo;
 		
 		public function __construct(){
-			
-			$this->pdo = new PDO("mysql:dbname=u270517400_ag;host=127.0.0.1","root","");
+			$this->pdo = new PDO("mysql:host=127.0.0.1;dbname=u270517400_ag;charset=utf8","root","");
 		}
 
 	}
